03ea8bdb84161103f6c16e1a819579cc46b916d3
Lee Mracek <lee.mracek@gmail.com>
2014-11-10 00:16:38 -0800

    initial commit of master

---
+++ src/main/java/com/m3rcuriel/Test.java
@@ -0,0 +1,23 @@
+package com.m3rcuriel;
+
+import org.bukkit.Location;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Lee Mracek on 11/9/14.
+ */
+public class Test extends JavaPlugin implements Listener, CommandExecutor{
+   public void onEnable() {
+       PluginManager pm = this.getServer().getPluginManager();
+       pm.registerEvents(this, this);
+   }
+
+   @EventHandler
+   public void onPlayerLogin(PlayerLoginEvent event) {
+       event.getPlayer().teleport(new Location(null, 0, 0, 0));
+   }
+}
